773c42c8efc1a760f8f346e992a440732a1ac8db
Yuri Ramos <yuri.pramos@gmail.com>
2020-04-14 10:16:29 -0300

    adding access_token id and dev_id

--- index.js
+++ index.js
@@ -1,11 +1,49 @@
+require('dotenv').config()
+const ACCESS_TOKEN = process.env.CLIENT_ACCESS_TOKEN;
+const APIAI_SESSION_ID = process.env.DEV_ACCESS_TOKEN;
+
+
+
+
 const express = require('express');
 const app = express();
 
 app.use(express.static(__dirname + '/views')); // html
 app.use(express.static(__dirname + '/public')); // js, css, images
 
-const server = app.listen(5000);
+const server = app.listen(process.env.PORT || 5000, () => {
+  console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
+});
 
+
+const apiai = require('apiai')(ACCESS_TOKEN);
+
+//WEB UI
 app.get('/', (req, res) => {
   res.sendFile('index.html');
+});
+
+const io = require('socket.io')(server);
+
+io.on('connection', function (socket) {
+  socket.on('chat message', (text) => {
+
+    // Get a reply from API.AI
+
+    let apiaiReq = apiai.textRequest(text, {
+      sessionId: APIAI_SESSION_ID
+    });
+
+    apiaiReq.on('response', (response) => {
+      let aiText = response.result.fulfillment.speech;
+      socket.emit('bot reply', aiText); // Send the result back to the browser!
+    });
+
+    apiaiReq.on('error', (error) => {
+      console.log(error);
+    });
+
+    apiaiReq.end();
+
+  });
 });
---
+++ public/js/script.js
@@ -0,0 +1,33 @@
+
+const socket = io();
+
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
+recognition.lang = 'en-US';
+recognition.interimResults = false;
+
+
+document.querySelector('button').addEventListener('click', () => {
+  recognition.start();
+});
+
+recognition.addEventListener('result', (e) => {
+  let last = e.results.length - 1;
+  let text = e.results[last][0].transcript;
+
+  console.log('Confidence: ' + e.results[0][0].confidence);
+
+  socket.emit('chat message', text);
+  // We will use the Socket.IO here later…
+});
+
+function synthVoice(text) {
+  const synth = window.speechSynthesis;
+  const utterance = new SpeechSynthesisUtterance();
+  utterance.text = text;
+  synth.speak(utterance);
+}
+
+socket.on('bot reply', function (replyText) {
+  synthVoice(replyText);
+});
