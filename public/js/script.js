e722eba9c43c34af389e0bde8c54653275ecc081
Yuri Ramos <yuri.pramos@gmail.com>
2020-04-14 10:26:00 -0300

    updates inside script

--- public/js/script.js
+++ public/js/script.js
@@ -1,24 +1,43 @@
+'use strict';
 
 const socket = io();
 
+const outputYou = document.querySelector('.output-you');
+const outputBot = document.querySelector('.output-bot');
+
 const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 const recognition = new SpeechRecognition();
+
 recognition.lang = 'en-US';
 recognition.interimResults = false;
-
+// recognition.maxAlternatives = 1;
 
 document.querySelector('button').addEventListener('click', () => {
   recognition.start();
 });
 
+recognition.addEventListener('speechstart', () => {
+  console.log('Speech has been detected.');
+});
+
 recognition.addEventListener('result', (e) => {
+  console.log('Result has been detected.');
+
   let last = e.results.length - 1;
   let text = e.results[last][0].transcript;
 
+  outputYou.textContent = text;
   console.log('Confidence: ' + e.results[0][0].confidence);
 
   socket.emit('chat message', text);
-  // We will use the Socket.IO here later…
+});
+
+recognition.addEventListener('speechend', () => {
+  recognition.stop();
+});
+
+recognition.addEventListener('error', (e) => {
+  outputBot.textContent = 'Error: ' + e.error;
 });
 
 function synthVoice(text) {
@@ -30,4 +49,7 @@ function synthVoice(text) {
 
 socket.on('bot reply', function (replyText) {
   synthVoice(replyText);
+
+  if (replyText == '') replyText = '(No answer...)';
+  outputBot.textContent = replyText;
 });
